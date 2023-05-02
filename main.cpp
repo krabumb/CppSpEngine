910b08dd9af2c05c0327f67af5a50e07df6db5a3
Clément LOSCOT <clement.loscot@telecomnancy.eu>
2023-05-02 22:31:47 +0200

    Add GLEW and GLFW

--- main.cpp
+++ main.cpp
@@ -1,36 +1,9 @@
-#include <iostream>
+#include <cstdio>
+#include <cstdlib>
 
-class Bogoss {
-private:
-    int x;
-    int y;
-
-public:
-    Bogoss(int x, int y) {
-        this->x = x;
-        this->y = y;
-    }
-
-    ~Bogoss()= default;
-
-    [[nodiscard]] int getX() const {
-        return x;
-    }
-
-    [[nodiscard]] int getY() const {
-        return y;
-    }
-};
-
-int getSumOfBogoss(const Bogoss *bg) {
-    return bg->getX() + bg->getY();
-}
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    Bogoss bg(1,2);
-    std::cout << bg.getX() << std::endl;
-    std::cout << bg.getY() << std::endl;
-    std::cout << getSumOfBogoss(&bg) << std::endl;
     return 0;
 }
